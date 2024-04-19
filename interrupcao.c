72ca00ee08b9363332756a45d1b9bef6c709b300
Fabricio Rodrigues <76504799+fab-rodrigs@users.noreply.github.com>
2024-04-18 21:05:22 -0300

    Create interrupcao.c

---
+++ interrupcao.c
@@ -0,0 +1,26 @@
+#include <msp430.h> 
+
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+
+	P1DIR = 0b00000001;       // configurar led como saída
+
+	// configura interrupções
+	P1REN |= 0b00000010;        // habilita resistor up down
+	P1OUT |= 0b00000010;        // habilita pull-up
+	P1IES = 0b00000010;                   // configura borda da interrupção
+	P1IE = 0b00000010;                    // habilita interrupção externa no P1.1
+	__bis_SR_register(GIE);     // habilita interrupções globais
+
+	while(1);
+}
+
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void)
+{
+    int i;
+    P1OUT ^= 0b00000001;  // piscar LED
+    for(i=1000000; i>0; i--);     //delay
+	P1IFG = 0; // limpa historico interrupção
+}
