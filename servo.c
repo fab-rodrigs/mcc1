de0f414b394e6084cd970dac087300a9f7f39c9a
Fabricio Rodrigues <76504799+fab-rodrigs@users.noreply.github.com>
2024-07-11 20:29:18 -0300

    Update servo.c

--- servo.c
+++ servo.c
@@ -11,23 +11,22 @@ int main(void)
 	P1SEL |= BIT2+BIT3;
 	TA0CCR0 = 20900-1;
 	TA0CCTL1 = OUTMOD_7;
-	TA0CCR1 = 523;
+	TABCCR1 = 523;
 	TA0CCTL2 = OUTMOD_7;
 	TA0CCR2 = 128;
 	TA0CTL = TASSEL_2 + MC_1 + TACLR;
-
+	
 	__no_operation();
 	
 	while(1)
 	{
-	    TA0CCR1 = 0;
+	    TA0CCR1 += 523;
 	    __delay_cycles(250000);
-	    TA0CCR1 = 523;
-	    //if(TA0CCR1 > 523)
-	    //{
-	    //    TA0CCR1 = 0;
-	    //}
+	    if(TA0CCR1 > 5*523)
+	    {
+	        TA0CCR1 = 0;
+	    }
 	}
-
+	    
 	return 0;
 }
